75b542ec121236801fa3848e009cd0b910a6c07b
ArthurFeed <Arthurresende2004@gmail.com>
2023-01-19 16:10:29 -0300

    Primeiras requisições, put e get

---
+++ JogosApi/Models/Jogo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JogosApi.Models
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Plataforma { get; set; }
+    }
+}
