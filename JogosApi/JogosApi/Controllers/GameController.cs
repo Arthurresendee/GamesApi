6ae380627e328eded0bea5b048eba89b6130485a
ArthurFeed <Arthurresende2004@gmail.com>
2023-01-20 17:29:39 -0300

    Criando requisições https sem banco de dados

--- JogosApi/JogosApi/Controllers/GameController.cs
+++ JogosApi/JogosApi/Controllers/GameController.cs
@@ -8,17 +8,48 @@ namespace JogosApi.Controllers
     public class GameController : ControllerBase
     {
         private static List<Game> games = new();
+        private static int _id = 1;
 
         [HttpPost]
-        public void AddGame([FromBody]Game game)
+        public string Add([FromBody]Game game)
         {
+            string mensagem = "Item Adicionado.";
             games.Add(game);
+            game.Id = _id;
+            _id++;
+            return mensagem;
         }
 
         [HttpGet]
-        public List<Game> GetGame()
+        public IEnumerable<Game> Get([FromQuery] int skip, int take = 50)
         {
-            return games;
+            return games.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public Game? GetById(int id)
+        {
+            return games.FirstOrDefault(game => game.Id == id);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id,[FromBody]Game newGame)
+        {
+            try
+            {
+                var game = games.First(g => g.Id == id);
+                if (game != null)
+                {
+                    game.Name = newGame.Name;
+                    game.Platform = newGame.Platform;
+                }
+                return Ok();
+            }
+            catch (InvalidOperationException ex) 
+            {
+                Console.WriteLine("sasa");
+                return NotFound();
+            }
         }
     }
 }
--- JogosApi/JogosApi/Models/Game.cs
+++ JogosApi/JogosApi/Models/Game.cs
@@ -1,9 +1,16 @@
-﻿namespace JogosApi.Models
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace JogosApi.Models
 {
     public class Game
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field name is required.")]
         public string Name { get; set; }
+
+        [StringLength(40)]
         public string? Platform { get; set; }
     }
 }
