02f27a15409a43446e8836b3c48d031bbb3bbfc7
ArthurFeed <Arthurresende2004@gmail.com>
2023-01-20 10:04:34 -0300

    Arruamndo Projeto

---
+++ JogosApi/JogosApi/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using JogosApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JogosApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class GameController : ControllerBase
+    {
+        private static List<Game> games = new();
+
+        [HttpPost]
+        public void AddGame([FromBody]Game game)
+        {
+            games.Add(game);
+        }
+
+        [HttpGet]
+        public List<Game> GetGame()
+        {
+            return games;
+        }
+    }
+}
---
+++ JogosApi/JogosApi/Models/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace JogosApi.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Platform { get; set; }
+    }
+}
